bd3272661eb7367abe5e027b7d48095de5140f1e
Paul-Elliot <peada@free.fr>
2026-05-31 11:36:06 +0200

    Minimal emacs mode with go_next and go_back

--- slipshow-eglot.el
+++ slipshow-eglot.el
@@ -4,8 +4,7 @@
   "Make a request to the server"
   (eglot-execute
    (eglot-current-server)
-   `(:command ,cmd :arguments ,args)
-  ))
+   `(:command ,cmd :arguments ,args)))
 
 (defun slipshow-preview-go-next ()
   "Go forward one step in the preview"
@@ -13,3 +12,12 @@
   (slipshow--execute-command
    "slipshow.go_next"
    (vector (eglot-path-to-uri (buffer-file-name)))))
+
+(defun slipshow-preview-go-previous ()
+  "Go forward one step in the preview"
+  (interactive)
+  (slipshow--execute-command
+   "slipshow.go_previous"
+   (vector (eglot-path-to-uri (buffer-file-name)))))
+
+(provide 'slipshow-eglot)
--- slipshow-mode.el
+++ slipshow-mode.el
@@ -1,10 +1,16 @@
 ;;; slipshow.el --- Main mode for Slipshow -*- coding: utf-8; lexical-binding: t -*-
 
-(require 'markdown-mode)
 (require 'slipshow-eglot)
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.slp\\'" . slipshow-mode))
+
+;;;###autoload
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '(slipshow-mode . "slipshow lsp")))
 
 ;;;###autoload
 (define-derived-mode slipshow-mode
   markdown-mode "Slipshow"
   "Major mode for editing slipshow files"
-  (slipshow-minor))
+  (eglot-ensure))
