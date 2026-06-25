4f7c9843e3469278210a330cdc5615a43f6c2af2
Paul-Elliot <peada@free.fr>
2026-06-25 14:43:27 +0200

    (Temporarily) move all config inside eglot file

--- slipshow-eglot.el
+++ slipshow-eglot.el
@@ -30,6 +30,24 @@
         ('on-key-stroke "Key stroke")
         ('on-save "Save")))))
 
+(defgroup slipshow nil
+  "Major mode for interacting with Slipshow."
+  :link '(url-link "https://slipshow.org")
+  :group 'languages
+  :prefix "slipshow-")
+
+(defun slipshow--set-and-resend-conf (symbol value)
+  "Set a config symbol and send the new conf to all slipshow eglot servers."
+  (set-default symbol value)
+  (slipshow-resend-conf))
+
+(defcustom slipshow-refresh-on 'on-key-stroke
+  "When to refresh the slipshow preview"
+  :group 'slipshow
+  :set #'slipshow--set-and-resend-conf
+  :type '(choice (const :tag "Refresh on every key stroke" on-key-stroke)
+                 (const :tag "Refresh on save" on-save)))
+
 (defun slipshow--set-dir-locals-var ()
   "Sets the dir locals variables"
   (dir-locals-set-class-variables
--- slipshow-mode.el
+++ slipshow-mode.el
@@ -2,24 +2,6 @@
 
 (require 'slipshow-eglot)
 
-(defgroup slipshow nil
-  "Major mode for interacting with Slipshow."
-  :link '(url-link "https://slipshow.org")
-  :group 'languages
-  :prefix "slipshow-")
-
-(defun slipshow--set-and-resend-conf (symbol value)
-  "Set a config symbol and send the new conf to all slipshow eglot servers."
-  (set-default symbol value)
-  (slipshow-resend-conf))
-
-(defcustom slipshow-refresh-on 'on-key-stroke
-  "When to refresh the slipshow preview"
-  :group 'slipshow
-  :set #'slipshow--set-and-resend-conf
-  :type '(choice (const :tag "Refresh on every key stroke" on-key-stroke)
-                 (const :tag "Refresh on save" on-save)))
-
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.slp\\'" . slipshow-mode))
 
