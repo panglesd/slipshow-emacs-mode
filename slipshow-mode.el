036f6539236ea15a9e3b05d7dc93891082d0b4f1
Paul-Elliot <peada@free.fr>
2026-06-25 15:39:52 +0200

    Add header to main file

--- slipshow-mode.el
+++ slipshow-mode.el
@@ -1,4 +1,17 @@
-;;; slipshow.el --- Main mode for Slipshow -*- coding: utf-8; lexical-binding: t -*-
+;;; slipshow-mode.el --- Main mode for Slipshow -*- coding: utf-8; lexical-binding: t -*-
+
+;; Copyright (C) 2026 Paul-Elliot Anglès d'Auriac
+
+;; Author: Paul-Elliot <peada@free.fr>
+;; Version: 0.1
+;; Package-Requires: ()
+;; Keywords: slipshow, presentation
+;; URL: https://slipshow.org
+
+;;; Commentary:
+
+;; This package provides support to slipshow. It defines a major mode for
+;; editing .slp files, start the preview server and communicate with it.
 
 (require 'slipshow-eglot)
 
