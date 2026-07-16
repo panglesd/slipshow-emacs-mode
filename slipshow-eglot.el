687182fd350bfae2c46c07b7183bcdc22e7059bf
Paul-Elliot <peada@free.fr>
2026-07-16 17:47:23 +0200

    slipshow namespace for experimental cap + lenienter in cap version

--- slipshow-eglot.el
+++ slipshow-eglot.el
@@ -10,31 +10,34 @@
 
 ;; Could be a function but earlier versions made sense as a macro (I wanted to
 ;; exercise my macro writing)
-(defmacro slipshow--check-capability (capability expected-value command-name payload error-msg)
+(defmacro slipshow--check-capability (capability check command-name payload error-msg)
   "Check if the capability exists with the right value, and if so, make an execute-command request with given name and payload"
-  `(let* ((capability-v (eglot-server-capable ,@capability)))
-    (if (equal capability-v ,expected-value)
+  `(let* ((capability-v (eglot-server-capable :experimental :slipshow ,@capability)))
+    (if (funcall ,check capability-v)
         (slipshow--execute-command ,command-name ,payload)
       (message ,error-msg))))
+
+(defconst slipshow--move-error-message
+  "The version of the Slipshow you have does not support editor-controlled movements. You need at least version 0.12.0.")
 
 (defun slipshow-preview-go-next ()
   "Go forward one step in the preview"
   (interactive)
   (slipshow--check-capability
-      (:experimental :move_from_editor :version)
-      1
+      (:move_from_editor :version)
+      #'identity
       "slipshow.go_next"
       (vector (eglot-path-to-uri (buffer-file-name)))
-    "The version of the Slipshow you have does not support editor-controlled movements. You need at least version 0.12.0."))
+    slipshow--move-error-message))
 
 (defun slipshow-preview-go-previous ()
   "Go backward one step in the preview"
   (interactive)
   (slipshow--check-capability
-      (:experimental :move_from_editor :version)
-      1
+      (:move_from_editor :version)
+      #'identity
       "slipshow.go_previous"
       (vector (eglot-path-to-uri (buffer-file-name)))
-    "The version of the Slipshow you have does not support editor-controlled movements. You need at least version 0.12.0."))
+    slipshow--move-error-message))
 
 (provide 'slipshow-eglot)
