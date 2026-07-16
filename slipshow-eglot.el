e4a611799e6c5b5b36818e56412053f24efdddca
Paul-Elliot <peada@free.fr>
2026-07-16 16:36:42 +0200

    Check that the server has the capability before sending requests

--- slipshow-eglot.el
+++ slipshow-eglot.el
@@ -8,18 +8,33 @@
    (eglot-current-server)
    `(:command ,cmd :arguments ,args)))
 
+;; Could be a function but earlier versions made sense as a macro (I wanted to
+;; exercise my macro writing)
+(defmacro slipshow--check-capability (capability expected-value command-name payload error-msg)
+  "Check if the capability exists with the right value, and if so, make an execute-command request with given name and payload"
+  `(let* ((capability-v (eglot-server-capable ,@capability)))
+    (if (equal capability-v ,expected-value)
+        (slipshow--execute-command ,command-name ,payload)
+      (message ,error-msg))))
+
 (defun slipshow-preview-go-next ()
   "Go forward one step in the preview"
   (interactive)
-  (slipshow--execute-command
-   "slipshow.go_next"
-   (vector (eglot-path-to-uri (buffer-file-name)))))
+  (slipshow--check-capability
+      (:experimental :move_from_editor :version)
+      1
+      "slipshow.go_next"
+      (vector (eglot-path-to-uri (buffer-file-name)))
+    "The version of the Slipshow you have does not support editor-controlled movements. You need at least version 0.12.0."))
 
 (defun slipshow-preview-go-previous ()
   "Go backward one step in the preview"
   (interactive)
-  (slipshow--execute-command
-   "slipshow.go_previous"
-   (vector (eglot-path-to-uri (buffer-file-name)))))
+  (slipshow--check-capability
+      (:experimental :move_from_editor :version)
+      1
+      "slipshow.go_previous"
+      (vector (eglot-path-to-uri (buffer-file-name)))
+    "The version of the Slipshow you have does not support editor-controlled movements. You need at least version 0.12.0."))
 
 (provide 'slipshow-eglot)
